8b8dcd01089120fc7b50f8487cf28be7e24c12d1
Camille Wintz <camille.wintz@web-education.net>
2016-06-17 14:14:46 +0200

    missing html and js files

---
+++ my-apps.js
@@ -0,0 +1,14 @@
+﻿(function () {
+    "use strict";
+
+    var widget = model.widgets.findWidget('my-apps');
+
+    http().get('/userbook/preference/apps').done(function (pref) {
+        widget.apps = JSON.parse(pref.preference);
+        model.widgets.apply();
+    });
+
+    widget.redirect = function (path) {
+        window.location.href = path;
+    };
+}());
