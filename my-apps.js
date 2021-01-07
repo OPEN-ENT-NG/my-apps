cbd98ee15392dbbb7d060af5f4db5559637e3130
richard.bunel@opendigitaleducation.com <richard.bunel@opendigitaleducation.com>
2021-01-07 20:43:25 +0100

    feat: #37753, use new bookmarks data model

--- my-apps.js
+++ my-apps.js
@@ -4,7 +4,12 @@
     var widget = model.widgets.findWidget('my-apps');
 
     http().get('/userbook/preference/apps').done(function (pref) {
-        widget.apps = JSON.parse(pref.preference);
+        var apps = JSON.parse(pref.preference);
+        widget.apps = apps && apps.bookmarks && apps.bookmarks.map(function(bookmark) {
+            return model.me.apps.find(function(app){
+                return app.name == bookmark;
+            });
+        });
         model.widgets.apply();
     });
 
